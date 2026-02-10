48e80963d96ace883c1b770a4700f497f87c31d7
roshanjha <roshankumanjha@gmail.com>
2026-02-10 08:58:40 +0530

    worked on the fibbonaci series

---
+++ fibonacci_series.py
@@ -0,0 +1,19 @@
+num=int(input("enter a number a :- "))
+
+n1,n2 = 0,1
+count = 0
+
+if num<=0:
+  print("please enter a positive number.")
+elif num == 1:
+  print("Fibonacci sequence upto",num,":")
+  print(n1)
+
+else:
+  while count<num:
+    print(n1)
+    nth = n1+n2
+    n1=n2
+    n2=nth
+    count +=1
+    
